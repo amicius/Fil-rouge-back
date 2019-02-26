c6e0560eaf6974d6b1ce734704bbfbe1ae1c7e5e
Desplat <matt-desplat@wanadoo.fr>
2019-02-26 09:42:58 +0100

    Premier commit: projet spring

---
+++ src/main/java/fr/dta/filrouge/FilrougeApplication.java
@@ -0,0 +1,13 @@
+package fr.dta.filrouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilrougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilrougeApplication.class, args);
+	}
+
+}
