195d24a67021beb413e3c7b9e3eaa76f947d3a23
bpayanpsl <bpayanpsl@gmail.com>
2019-02-27 10:35:46 +0100

    Debug game

---
+++ src/main/java/fr/dta/entity/Game.java
@@ -0,0 +1,51 @@
+package fr.dta.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Game {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private String nom;
+	
+	@Column
+	private Plateform plateforme;
+	
+	@Column
+	private Genre genre;
+	
+	@Column(nullable = false)
+	private double prix;
+	
+	@Column
+	private String image;
+	
+	@Column
+	private String description;
+	
+	@Column(nullable = false)
+	private String reference;
+	
+	@Column(nullable = false)
+	private int stock;
+	
+	@Column
+	private String editeur;
+	
+	@Column
+	private Date dateParution;
+	
+	@Column
+	private boolean actif;
+
+}
