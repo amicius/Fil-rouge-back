5a2babd33e0ed532b8d762b5c08070f788048a60
bpayanpsl <bpayanpsl@gmail.com>
2019-02-27 10:11:09 +0100

    Création de l'entitée Jeu

--- src/main/java/fr/dta/entity/Jeu.java
+++ src/main/java/fr/dta/entity/Jeu.java
@@ -1,5 +1,8 @@
 package fr.dta.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,10 +15,37 @@ public class Jeu {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	@Column(nullable = false)
 	private String nom;
-
+	
+	@Column
 	private Plateforme plateforme;
-
+	
+	@Column
 	private Genre genre;
+	
+	@Column(nullable = false)
+	private double prix;
+	
+	@Column
+	private String image;
+	
+	@Column
+	private String description;
+	
+	@Column(nullable = false)
+	private String reference;
+	
+	@Column(nullable = false)
+	private int stock;
+	
+	@Column
+	private String editeur;
+	
+	@Column
+	private Date dateParution;
+	
+	@Column
+	private boolean actif;
 
 }
