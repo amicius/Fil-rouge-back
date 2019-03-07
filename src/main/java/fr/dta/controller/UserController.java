3c4b12f3b028a327b343a25e7d390da8acad3731
Desplat <matt-desplat@wanadoo.fr>
2019-03-07 15:05:54 +0100

    Ajout du logout

--- src/main/java/fr/dta/controller/UserController.java
+++ src/main/java/fr/dta/controller/UserController.java
@@ -2,11 +2,17 @@ package fr.dta.controller;
 
 import java.security.Principal;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +41,24 @@ public class UserController {
 			return new ResponseEntity<>(user, HttpStatus.OK);
 		}
 		return ResponseEntity.badRequest().build();
+	}
+
+	@GetMapping("/logout")
+	public void logout(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		if (request.isRequestedSessionIdValid() && session != null) {
+			session.invalidate();
+		}
+
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				cookie.setMaxAge(0);
+				cookie.setValue(null);
+				cookie.setPath("/");
+				response.addCookie(cookie);
+			}
+		}
 	}
 
 	@PostMapping("/create-account")
